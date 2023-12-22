51dff0816f016ca6451ce2ff0613dfe67758021a
BaluNeu <mundlamuri.b@northeastern.edu>
2023-12-22 14:32:11 -0500

    convertor done

--- python/src/converter/consumer.py
+++ python/src/converter/consumer.py
@@ -15,12 +15,30 @@ def main():
 
     # rabbitmq connection
     connection = pika.BlockingConnection(pika.ConnectionParameters(host = "rabbitmq"))
-
-    #whenever a message is taken by the queue this function is being executed
-    def callback():
-
-
     channel = connection.channel()
 
+    #whenever a message is taken by the queue this function is being executed
+    def callback(ch,method,properties,body):
+        err = to_mp3.start(body,fs_videos,fs_mp3s,ch)
+        if err:
+            ch.basic_nack(delivery_tag = method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+
     # consume the messages from video queue
-    channel.basic_consume(queue = os.environ.get("VIDEO_QUEUE"), on_message_callback = callback)    
+    channel.basic_consume(queue = os.environ.get("VIDEO_QUEUE"), on_message_callback = callback)  
+
+    # start consuming
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)  
+
+
